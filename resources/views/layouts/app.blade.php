3cc48468e48af6f5db5269b4d51b45990cc3a50c
danendrarafi01 <danendrarafi01@gmail.com>
2026-01-02 03:26:36 +0700

    menambahkan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,8 @@
 
     </div>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     {{-- CONTENT --}}
     <div class="content">
         @yield('content')
