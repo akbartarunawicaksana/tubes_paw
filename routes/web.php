89da55a7c0bb71c032a93d8dbf729cffdb41d4da
danendrarafi01 <danendrarafi01@gmail.com>
2026-04-13 20:38:22 +0700

    ngehapus api

--- routes/web.php
+++ routes/web.php
@@ -78,16 +78,3 @@ Route::get('/pengaturan/profil/edit', function () {
 })->name('profil.edit');
 
 
-// ================= API =================
-
-Route::prefix('api')->group(function () {
-
-    // PRODUK API
-    Route::get('produk', [ProductController::class, 'index']);
-    Route::post('produk', [ProductController::class, 'store']);
-    Route::put('produk/{id}', [ProductController::class, 'update']);
-    Route::delete('produk/{id}', [ProductController::class, 'destroy']);
-
-    // PUBLIC API (WAJIB DOSEN)
-    Route::get('external', [ProductController::class, 'externalApi']);
-});
